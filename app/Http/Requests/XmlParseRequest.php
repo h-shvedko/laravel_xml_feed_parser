1f4734118fbe8ef0c75ce39416fa00d797165805
Mac <first@dizain.org>
2022-05-31 14:18:17 +0200

    added base Api request

--- app/Http/Requests/XmlParseRequest.php
+++ app/Http/Requests/XmlParseRequest.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
-
-class XmlParseRequest extends FormRequest
+class XmlParseRequest extends BaseApiRequest
 {
     /**
      * Determine if the user is authorized to make this request.
